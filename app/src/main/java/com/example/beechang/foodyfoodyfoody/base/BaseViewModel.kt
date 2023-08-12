2208fdb9ba20f951c6005c385c6705747e3e7023
beechang <dbsckd92@naver.com>
2023-08-13 01:44:36 +0900

    EDIT change ResultError -> ApiException

--- app/src/main/java/com/example/beechang/foodyfoodyfoody/base/BaseViewModel.kt
+++ app/src/main/java/com/example/beechang/foodyfoodyfoody/base/BaseViewModel.kt
@@ -1,20 +1,22 @@
 package com.example.beechang.foodyfoodyfoody.base
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
-import com.example.beechang.foodyfoodyfoody.model.ResultError
-import kotlinx.coroutines.flow.Flow
+import com.example.beechang.foodyfoodyfoody.model.ApiException
+ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
 
 open class BaseViewModel : ViewModel() {
 
     protected fun <T> Flow<T>.handleErrors(action: suspend (errorMassage: String) -> Unit): Flow<T> =
         catch { e ->
-            var errorMassage = "기타 오류 발생"
+            Log.e("e" , e.message.toString())
+            var errorMassage = "기타 오류 발생 " + e.message
             when (e) {
-                is ResultError.TokenExpired -> errorMassage = "인증문제 발생"
-                is ResultError.NotFound -> errorMassage = "잘못된 경로 요청"
-                is ResultError.ServerError -> errorMassage = "연결 서버의 문제발생"
-                is ResultError.OthersError -> errorMassage = "네트워크 오류발생"
+                is ApiException.TokenExpired -> errorMassage = "인증문제 발생 "
+                is ApiException.NotFound -> errorMassage = "잘못된 경로 요청 "
+                is ApiException.ServerError -> errorMassage = "연결 서버의 문제발생"
+                is ApiException.OthersError -> errorMassage = "네트워크 오류발생"
             }
             action(errorMassage)
         }
