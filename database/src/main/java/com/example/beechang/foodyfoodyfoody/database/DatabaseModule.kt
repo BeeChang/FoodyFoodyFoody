f13d1a980d83b0d2f33237dfa6dbbac23e5fb9fb
beechang <dbsckd92@naver.com>
2023-08-13 01:32:50 +0900

    DELETE wrong import

--- database/src/main/java/com/example/beechang/foodyfoodyfoody/database/DatabaseModule.kt
+++ database/src/main/java/com/example/beechang/foodyfoodyfoody/database/DatabaseModule.kt
@@ -2,7 +2,6 @@ package com.example.beechang.foodyfoodyfoody.database
 
 import android.content.Context
 import androidx.room.Room
-import com.example.foodyfoody.database.TypeResponseConverter
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import dagger.Module
