bd008e90f83c561de1365ae47bca499688f54292
beechang <dbsckd92@naver.com>
2023-08-22 16:38:46 +0900

    EDIT room db migration1to2

--- database/src/main/java/com/example/beechang/foodyfoodyfoody/database/RecipesDatabase.kt
+++ database/src/main/java/com/example/beechang/foodyfoodyfoody/database/RecipesDatabase.kt
@@ -6,7 +6,7 @@ import androidx.room.TypeConverters
 
 @Database(
     entities = [RecipesEntity::class, FavoritesEntity::class ],
-    version = 1,
+    version = 2,
     exportSchema = false ,
 )
 @TypeConverters(value = [TypeResponseConverter::class])
